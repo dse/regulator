5568f1b00b14da0fcffcf31affc46596ba525863
Darren Embry <dse@webonastick.com>
2019-07-01 16:32:19 -0400

    continued work

--- regulator.h
+++ regulator.h
@@ -1,16 +1,27 @@
 #ifndef REGULATOR_H
 #define REGULATOR_H
 
-void regulator_init();
-void regulator_pulseaudio_init();
-void regulator_sndfile_init();
-void regulator_usage();
+#include <unistd.h>
+
+typedef struct regulator_sample_t {
+    int16_t sample;
+    size_t  index;
+} regulator_sample_t;
+
 void regulator_run();
-void regulator_test();
-void regulator_guess();
+void regulator_pulseaudio_open();
+void regulator_sndfile_open();
+size_t regulator_read();
+size_t regulator_pulseaudio_read();
+size_t regulator_sndfile_read();
+void regulator_analyze_tick();
+void regulator_usage();
 void regulator_options(int *argcp, char * const **argvp);
-void regulator_read_buffer();
 void regulator_show_vu();
+
+int sample_sort(const regulator_sample_t *a,
+                const regulator_sample_t *b);
+int size_t_sort(const size_t *a, const size_t *b);
 
 #include <stdint.h>
 
