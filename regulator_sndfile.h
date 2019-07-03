4583ea5972c4e553165b7594498911d1aa4d571b
Darren Embry <dse@webonastick.com>
2019-07-03 13:54:55 -0400

    dos2unix

--- regulator_sndfile.h
+++ regulator_sndfile.h
@@ -7,6 +7,7 @@
 
 void regulator_sndfile_open(struct regulator_t* rp);
 void regulator_sndfile_close(struct regulator_t* rp);
-size_t regulator_sndfile_read(struct regulator_t* rp, int16_t* ptr, size_t samples);
+size_t regulator_sndfile_read(struct regulator_t* rp,
+                              int16_t* ptr, size_t samples);
 
 #endif  /* REGULATOR_SNDFILE_H */
