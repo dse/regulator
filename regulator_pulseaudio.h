f076fd4c704c41dc22719ca67baf2cd467ea6af5
Darren Embry <dse@webonastick.com>
2019-07-03 23:34:46 -0400

    test vu

--- regulator_pulseaudio.h
+++ regulator_pulseaudio.h
@@ -10,4 +10,7 @@ void regulator_pulseaudio_close(struct regulator_t* rp);
 size_t regulator_pulseaudio_read(struct regulator_t* rp,
                                  int16_t* ptr, size_t samples);
 
+void regulator_pulseaudio_test(struct regulator_t* rp);
+void regulator_pulseaudio_vu(struct regulator_t* rp, int16_t* buffer, size_t frames);
+
 #endif  /* REGULATOR_PULSEAUDIO_H */
